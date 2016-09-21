5a1ba83b7dd1d85450086def89942e22febf65f7
jordanalexq <jordan.quintana@edtric.com>
2016-09-20 20:54:45 -0700

    More changes
    Up to date version

--- L3GD20_SPI.c
+++ L3GD20_SPI.c
@@ -12,17 +12,25 @@
 
 #include "L3GD20_SPI.h"
 
+uint8 txBuffer[10];
+uint8 rxBuffer[10];
+
 uint8 Gyro_WAI( void )
 {
     uint8 ID;
     uint8 myval = ( READ | ADDR_CNST | L3GD20_REGISTER_WHO_AM_I );
-    
-    SLAVE1_ON;
+  
     SPI_ClearTxBuffer();
     SPI_WriteTxData( myval );
     SPI_WriteTxData( 0x00 );
+    
+//    txBuffer[0] = myval;
+//    txBuffer[1] = 0x00;
+//    SPI_PutArray( txBuffer , 2 );
     ID = SPI_ReadRxData();
-    SLAVE1_OFF;
+//    CyDelay(5);
+    SPI_ClearRxBuffer();
+    
     
     return ID;
 }
